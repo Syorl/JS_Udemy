45aa13e13325bed70df431cf775b7df2c5b0a363
Syorl <guiornellas14@gmail.com>
2024-01-21 21:13:00 -0300

    consts e let

--- aula01/index.js
+++ aula01/index.js
@@ -1,3 +1,9 @@
 //output
 // console.log('Hi world'); 
 console.log('Meu nome é "Guilherme Ornellas.', 'Estou aprendendo JS às',16,'da tarde.')
+// variaveis
+let nome = 'joao';
+let ano = 1980;
+console.log(nome,'é pai de José que nasceu em',ano)
+// constantes (são as mesmas regras das váriaveis)
+const nascimento = 1988;
