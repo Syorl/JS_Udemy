dd19bc235dfd8d069a5467f99b64f4dfc1e2d5ba
Syorl <guiornellas14@gmail.com>
2024-02-02 10:24:34 -0300

    contadores

--- aula01/index.js
+++ aula01/index.js
@@ -1,13 +1,46 @@
 //output
 // console.log('Hi world'); 
-console.log('Meu nome é "Guilherme Ornellas.', 'Estou aprendendo JS às',16,'da tarde.')
-// variaveis
+// console.log('Meu nome é "Guilherme Ornellas.', 'Estou aprendendo JS às',16,'da tarde.')
+// // variaveis
+
 // let nome = 'joao';
 // let ano = 1980;
-console.log(nome,'é pai de José que nasceu em',ano)
+// console.log(nome,'é pai de José que nasceu em',ano);
 // constantes (são as mesmas regras das váriaveis)
-const nome = 'Guilherme';
-const sobrenome = 'Ornellas';
-const idade = 18;
-const peso = 82;
-const alturaEmM= 1.80;
+
+// const nome = 'Guilherme';
+// const sobrenome = 'Ornellas';
+// const idade = 18;
+// const peso = 82;
+// const alturaEmM= 1.80;
+
+// Exercicio 1
+
+/*
+Luiz Otávio Miranda tem 30 anos, pesa 84 kg
+tem 1.8 de altura e seu IMC é de 25.925925925925924
+Luiz Otávio nasceu em 1980
+*/
+    // const nome = 'Luiz Otávio';
+    // const sobrenome = 'Miranda';
+    // const idade = 30;
+    // const peso = 84;
+    // const alturaEmM = 1.80; // Corrigido para metros (ao invés de cm)
+    // let indiceMassaCorporal; // peso / (altura * altura)
+    // let anoNascimento;
+
+
+
+//console.log(123)
+
+/*Operadores aritiméticos
++ adição || concatenação
+- sub
+/ div
+** potencia
+% resto da div
+*/
+// const n1 = 5;
+// const n2 = 10;
+// console.log('O resultado é: '+n1*n2)
+
